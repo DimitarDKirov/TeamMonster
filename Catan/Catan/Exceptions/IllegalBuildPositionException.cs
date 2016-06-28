821319e59980be0ebfaa54e8ca32ca169f1ce193
silweto7 <silweto7@abv.bg>
2016-06-29 00:38:46 +0300

    Upload Exceptions and the dices

---
+++ Catan/Catan/Exceptions/IllegalBuildPositionException.cs
@@ -0,0 +1,22 @@
+﻿namespace Catan.Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Thrown when a build fails because it's in an illegal position
+    /// </summary>
+    class IllegalBuildPositionException : AgentActionException
+    {
+        public IllegalBuildPositionException()
+        {
+        }
+        public IllegalBuildPositionException(string message)
+            : base(message)
+        {
+        }
+    }
+}
