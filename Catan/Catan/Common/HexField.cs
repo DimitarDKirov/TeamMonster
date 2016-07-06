5755d71884cbfc4157d41f85a5d88e94f90927eb
silweto7 <silweto7@abv.bg>
2016-07-06 17:20:52 +0300

    Added hexField

---
+++ Catan/Catan/Common/HexField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWorkProjectOOP.HexFields
+{
+    class HexField
+    {
+    }
+}
