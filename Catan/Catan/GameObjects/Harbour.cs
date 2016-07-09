53762656f7c8f7bc701a4978c6cff1ddd7ec4946
Simeon Tzvetkov <simeon_tzvetkov@mail.bg>
2016-07-09 13:54:28 +0300

    Trade implemented.

--- Catan/Catan/GameObjects/Harbour.cs
+++ Catan/Catan/GameObjects/Harbour.cs
@@ -14,7 +14,7 @@ namespace Catan.GameObjects
         private ResourceType tradingResource;
 
 
-       
+
         // properties
         public ResourceType TradingResource
         {
@@ -29,6 +29,36 @@ namespace Catan.GameObjects
         }
 
         // methods
-     
+
+        public static void Trade(IPlayer playerOnTurn, ResourceType offerredResource, ResourceType targetResource)
+        {
+            uint tradeRate=4;
+            if (!HarbourExists(playerOnTurn, targetResource))
+            {
+                tradeRate = 2;
+            }
+            else if (!HarbourExists(playerOnTurn, ResourceType.None0))
+            {
+                tradeRate = 3;
+            }
+            uint offeredResourceAvailable = playerOnTurn.GetResourceValue(offerredResource);
+            if (offeredResourceAvailable < tradeRate)
+            {
+                throw new Exception("Not enough resources!"); // TOD: custime exception
+            }
+
+            playerOnTurn.SetResourceValue(offerredResource, (offeredResourceAvailable-tradeRate));
+            playerOnTurn.SetResourceValue(targetResource, (playerOnTurn.GetResourceValue(targetResource)+1));
+        }
+
+        private static bool HarbourExists(IPlayer playerOnTurn, ResourceType harbourResourceType)
+        {
+            foreach (var harbour in playerOnTurn.Harbours)
+            {
+                if (harbour.TradingResource == harbourResourceType)
+                    return true;
+            }
+            return false;
+        }
     }
 }
